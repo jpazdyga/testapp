cdc8180642c148b5fb9e27f8c1e12fdc5d892f72
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-08-07 09:28:06 +0100

    Update

--- dev/root_sample/index.php
+++ dev/root_sample/index.php
@@ -6,7 +6,7 @@ echo "<html><b><br />This is an ugly PHP installation test page.<br /><b/>";
 echo "<body>";
 
 echo "
-<div class="CSS_Table_Example" style="width:600px;height:150px;">
+<div class='CSS_Table_Example' style='width:600px;height:150px;'>
 <table>
 <tr> 
 <td>
