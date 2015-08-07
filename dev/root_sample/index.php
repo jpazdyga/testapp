9384d5258e21d5290c1ea2429629fcdefcf865df
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-08-07 09:26:13 +0100

    Update

--- dev/root_sample/index.php
+++ dev/root_sample/index.php
@@ -1,4 +1,21 @@
 <?php
 echo "<link href='./styles/style.css' rel='stylesheet' type='text/css' />";
-echo "<html><b><br />This is an ugly PHP installation test page.<br /><b/></html>";
+echo "<link href='./styles/table.css' rel='stylesheet' type='text/css' />";
+echo "<html><b><br />This is an ugly PHP installation test page.<br /><b/>";
+
+echo "<body>";
+
+echo "
+<div class="CSS_Table_Example" style="width:600px;height:150px;">
+<table>
+<tr> 
+<td>
+Title 1
+</td>
+</tr>
+</table>
+
+";
+echo "</body>";	
+echo "</html>";
 ?>
