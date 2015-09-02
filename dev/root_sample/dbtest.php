f16a7765ec08fa96d90641d7d5332fe0e860b0a2
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-09-02 20:29:24 +0100

    Added simple and quick database test

--- dev/root_sample/dbtest.php
+++ dev/root_sample/dbtest.php
@@ -10,4 +10,17 @@ mysql_connect(dbhost,$username,$password);
 $query="SELECT * FROM contacts";
 $result=mysql_query($query);
 
+$num=mysql_numrows($result);
+
+mysql_close();
+
+echo "<b><center>Database Output</center></b><br><br>";
+
+$i=0;
+while ($i < $num) {
+$first=mysql_result($result,$i,"first");
+echo "<b>$first</b><br />";
+$i++;
+}
+
 ?>
