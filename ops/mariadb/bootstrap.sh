2bf46adad55570f6e6440843f9367e51542f30bb
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-09-03 13:13:09 +0100

    Fixed bootstrap.sh for mariadb helper

--- ops/mariadb/bootstrap.sh
+++ ops/mariadb/bootstrap.sh
@@ -1,5 +1,5 @@
-export TERM=xterm;
-sleep 2
+export TERM=xterm
 /usr/bin/mysqld_safe &
-mysqladmin --protocol=tcp -hlocalhost password test11;
-mysql --protocol=tcp -hlocalhost -uroot -ptest11 -e "CREATE USER 'dbuser' IDENTIFIED BY 'dbpass'; create database testapp_db; GRANT ALL PRIVILEGES ON testapp_db.* TO 'dbuser'@'%' IDENTIFIED BY 'dbpass' WITH GRANT OPTION; RANT ALL PRIVILEGES ON testapp_db.* TO 'dbuser'@'localhost' IDENTIFIED BY 'dbpass' WITH GRANT OPTION; FLUSH PRIVILEGES;";
+sleep 3
+mysqladmin --protocol=tcp -hlocalhost password test11
+mysql --protocol=tcp -hlocalhost -uroot -ptest11 -e "create database testapp_db; GRANT ALL PRIVILEGES ON testapp_db.* TO 'dbuser'@'%' IDENTIFIED BY 'dbpass' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON testapp_db.* TO 'dbuser'@'localhost' IDENTIFIED BY 'dbpass' WITH GRANT OPTION; FLUSH PRIVILEGES;"
